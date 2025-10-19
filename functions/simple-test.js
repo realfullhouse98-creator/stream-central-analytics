d5787f931ecf862a74f5552e24b84efaf8d82de8
realfullhouse98-creator <realfullhouse98@gmail.com>
2025-10-19 19:09:21 +0200

    Delete functions directory

--- functions/simple-test.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ 
-      message: "🎉 Netlify Functions are WORKING!",
-      timestamp: new Date().toISOString()
-    })
-  };
-};
