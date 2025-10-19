5c916d92df595ec834996461db59359d757d5734
realfullhouse98-creator <realfullhouse98@gmail.com>
2025-10-19 19:13:32 +0200

    Update simple-test.js

--- api/simple-test.js
+++ api/simple-test.js
@@ -1,1 +1,7 @@
-
+export default async function handler(request, response) {
+  return response.status(200).json({
+    message: "🎉 Vercel Functions are WORKING!",
+    timestamp: new Date().toISOString(),
+    platform: "Vercel Serverless"
+  });
+}
