24dac95e4a9180fee6bf45b647e7df9f7182f256
realfullhouse98-creator <realfullhouse98@gmail.com>
2025-11-07 14:17:16 +0200

    Update match-reaction.js

--- modules/match-reaction.js
+++ modules/match-reaction.js
@@ -1,27 +1,5 @@
 class MatchReaction {
     constructor(communityManager) {
         this.communityManager = communityManager;
-        this.isInitialized = false;
-    }
-
-    async init() {
-        if (this.isInitialized) return true;
-        this.isInitialized = true;
-        return true;
-    }
-
-    show() {
-        const container = document.getElementById('dynamic-content');
-        container.innerHTML = `
-            <div class="content-section">
-                <div class="navigation-buttons">
-                    <button class="home-button">⌂</button>
-                    <button class="top-back-button">←</button>
-                </div>
-                <div>Match Reaction</div>
-            </div>
-        `;
-        document.querySelector('.home-button').onclick = () => this.communityManager.matchScheduler.uiManager.showMainMenu();
-        document.querySelector('.top-back-button').onclick = () => this.communityManager.showMainMenu();
     }
 }
