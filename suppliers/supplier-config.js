c2705db7c123d82e632c48e167a0d900d6a37974
realfullhouse98-creator <realfullhouse98@gmail.com>
2025-11-20 10:06:53 +0200

    Create supplier-config.js

---
+++ suppliers/supplier-config.js
@@ -0,0 +1,12 @@
+// suppliers/supplier-config.js
+module.exports = {
+  tom: {
+    name: 'tom',
+    file: './suppliers/tom-data.json'
+  },
+  sarah: {
+    name: 'sarah', 
+    file: './suppliers/sarah-data.json'
+  }
+  // Add new suppliers here
+};
